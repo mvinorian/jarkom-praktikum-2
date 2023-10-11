52d19e1719951e5bfdf693a39d43f378c7b44c11
mvinorian <vinorianmuhammad@gmail.com>
2023-10-11 22:36:09 +0700

    feat: add parikesit.abimanyu.a01.com

--- setup.sh
+++ setup.sh
@@ -2,19 +2,19 @@ export DEBIAN_FRONTEND=noninteractive
 
 apt-get install nginx php php-fpm -y
 
-mkdir -p /var/www/arjuna.a01.com
-mkdir -p /var/www/abimanyu.a01.com
-
 cp -r ~/arjuna.a01.com /var/www
 cp -r ~/abimanyu.a01.com /var/www
+cp -r ~/parikesit.abimanyu.a01.com /var/www
 
 cp ~/nginx/arjuna.a01.com /etc/nginx/sites-available/arjuna.a01.com
 cp ~/apache2/abimanyu.a01.com.conf /etc/apache2/sites-available/abimanyu.a01.com.conf
+cp ~/apache2/parikesit.abimanyu.a01.com.conf /etc/apache2/sites-available/parikesit.abimanyu.a01.com.conf
 
 ln -sf /etc/nginx/sites-available/arjuna.a01.com /etc/nginx/sites-enabled
 rm -f /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
 
 a2ensite abimanyu.a01.com
+a2ensite parikesit.abimanyu.a01.com
 a2enmod rewrite
 
 service apache2 restart
