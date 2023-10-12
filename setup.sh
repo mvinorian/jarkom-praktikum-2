d0ce7ea1980a2fbce7b88dad652dd36b9149e9e7
mvinorian <vinorianmuhammad@gmail.com>
2023-10-12 08:32:29 +0700

    feat: add rjp.baratayuda.abimanyu.a01.com

--- setup.sh
+++ setup.sh
@@ -5,16 +5,19 @@ apt-get install nginx php php-fpm git -y
 cp -r ~/arjuna.a01.com /var/www
 cp -r ~/abimanyu.a01.com /var/www
 cp -r ~/parikesit.abimanyu.a01.com /var/www
+cp -r ~/rjp.baratayuda.abimanyu.a01.com /var/www
 
 cp ~/nginx/arjuna.a01.com /etc/nginx/sites-available/arjuna.a01.com
 cp ~/apache2/abimanyu.a01.com.conf /etc/apache2/sites-available/abimanyu.a01.com.conf
 cp ~/apache2/parikesit.abimanyu.a01.com.conf /etc/apache2/sites-available/parikesit.abimanyu.a01.com.conf
+cp ~/apache2/rjp.baratayuda.abimanyu.a01.com.conf /etc/apache2/sites-available/rjp.baratayuda.abimanyu.a01.com.conf
 
 ln -sf /etc/nginx/sites-available/arjuna.a01.com /etc/nginx/sites-enabled
 rm -f /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
 
 a2ensite abimanyu.a01.com
 a2ensite parikesit.abimanyu.a01.com
+a2ensite rjp.baratayuda.abimanyu.a01.com
 a2enmod rewrite
 
 service apache2 restart
